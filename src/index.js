30f5c3cd959b4a1eea2784476ed411eba88ab473
0xkumi <dung.q@autonomous.nyc>
2018-09-25 17:51:18 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import PostsIndex from './components/posts_index'
 
-import App from './components/app';
-import reducers from './reducers';
+import App from './components/app'
+import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container')
+)
